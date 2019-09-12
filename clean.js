db98afd626e6a9d929ce9652548c44be82146ec4
muthuramanct <ctmuthu93@gmail.com>
2019-09-13 00:42:08 +0200

    npm clean

---
+++ clean.js
@@ -0,0 +1,20 @@
+try {
+	const fs = require("fs");
+	const del = require('del');
+	try {
+			if (fs.existsSync("package-lock.json")) {
+				del('package-lock.json');
+			}
+			if (fs.existsSync("dist")) {
+				del('dist');
+			}
+			if (fs.existsSync("node_modules")) {
+				del('node_modules');
+			}
+			console.log('Successfully Cleaned');
+	} catch (err) {
+			console.log('Unable to clean');
+	}
+} catch (err) {
+	console.log('node_modules folder not found');
+}
