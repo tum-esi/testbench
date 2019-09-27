38223857330da8a552d98ab7bf0e0d56826f9f5d
muthuramanct <ctmuthu93@gmail.com>
2019-09-27 17:31:53 +0200

    Test script to test git push

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm run build
-npm start testing-files/perfectThing.js &
-node testing-files/perfectThing.js &
-sleep 5s
-curl -s -X GET http://localhost:8980/wot-test-bench/properties/testData
